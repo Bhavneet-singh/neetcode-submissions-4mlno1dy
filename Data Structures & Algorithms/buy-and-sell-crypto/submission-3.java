4edddc05b2e7accf6d00b7ca239bddae9a7ffa08
bhavcodes <109847685+Bhavneet-singh@users.noreply.github.com>
2026-08-13 15:18:46 +0530

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        // 
+        int profit = 0 ; 
+        int left = 0 , right = 1 ;
+        int n = prices.length ; 
+
+        while(right < n ) {
+            //             System.out.println("left" +left) ; 
+            //                                     System.out.println(" ") ; 
+
+
+            // System.out.println("right" + right) ; 
+            if(prices[left] < prices[right]){
+                profit = Math.max(profit , (prices[right] - prices[left])) ; 
+            }
+            else{
+                left = right  ; 
+            }
+            right ++  ;
+
+        }
+        return profit ; 
+        
+
+        
+    }
+}
